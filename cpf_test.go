625534518566ca7fb0044265eb4d667d540d4701
Lucas Klassmann <lucasklassmann@gmail.com>
2018-06-21 10:54:09 -0300

    Added tests for the new CPF type

--- cpf_test.go
+++ cpf_test.go
@@ -59,3 +59,45 @@ func TestValidCPFWithInvalidNumber3(t *testing.T) {
 		t.Errorf("Invalid result: %v", r)
 	}
 }
+
+func TestCPFTypeIsValidWithValidNumber(t *testing.T) {
+	var cpf CPF = "71656686759"
+
+	if !cpf.IsValid() {
+		t.Errorf("Invalid result: false")
+	}
+}
+
+func TestCPFTypeIsValidWithInvalidNumber(t *testing.T) {
+	var cpf CPF = "71656686734"
+
+	if cpf.IsValid() {
+		t.Errorf("Invalid result: true")
+	}
+}
+
+func TestCPFTypeStringFormattingWithValidNumber(t *testing.T) {
+	var cpf CPF = "71656686759"
+	r := cpf.String()
+
+	if r != "716.566.867-59" {
+		t.Errorf("Invalid result: %v", r)
+	}
+}
+
+func TestCPFTypeStringFormattingWithInvalidNumber(t *testing.T) {
+	var cpf CPF = "71656686734"
+	r := cpf.String()
+
+	if r != "71656686734" {
+		t.Errorf("Invalid result: %v", r)
+	}
+}
+
+func TestCPFNewCPF(t *testing.T) {
+	cpf := NewCPF("716.566.867-59")
+
+	if cpf != "71656686759" {
+		t.Errorf("Invalid result: %v", cpf)
+	}
+}
